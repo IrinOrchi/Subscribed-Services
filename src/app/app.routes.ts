963c06834389c6c08789332075ddc9652466cb75
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-06-26 15:59:36 +0600

    subscribed services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,17 @@
 import { Routes } from '@angular/router';
-import { EditAccountPageComponent } from './pages/edit-account-page/edit-account-page.component';
-import { SuccessfulAccountComponent } from './pages/successful-account/successful-account.component';
 import { AuthGuard } from './guards/auth.guard';
+import { SubscribedServicesComponent } from './pages/subscribed-services/subscribed-services.component';
 
 export const routes: Routes = [
     {
         path:"",
-        redirectTo: 'EditAccount',
+        redirectTo: 'SubscribedServices',
         pathMatch: 'full'
     },
+  
     {
-        path:'EditAccount',
-        component: EditAccountPageComponent,
-        // canActivate: [AuthGuard]
-    },
-    {
-        path: "account-updated-successfully",
-        component: SuccessfulAccountComponent,
+        path: "SubscribedServices",
+        component: SubscribedServicesComponent,
         // canActivate: [AuthGuard]
     },
    
---
+++ src/app/pages/subscribed-services/subscribed-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribed-services',
+  imports: [],
+  standalone: true,
+  templateUrl: './subscribed-services.component.html',
+  styleUrl: './subscribed-services.component.scss'
+})
+export class SubscribedServicesComponent {
+
+}
