1f0aa4234b88373877717ecaf8a10b493bd130e0
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-06-28 13:03:29 +0600

    design updated

--- src/app/pages/subscribed-services/subscribed-services.component.ts
+++ src/app/pages/subscribed-services/subscribed-services.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateRangePickerModalComponent } from '../../components/date-range-picker-modal/date-range-picker-modal.component';
 
 @Component({
   selector: 'app-subscribed-services',
-  imports: [],
+  imports: [CommonModule, DateRangePickerModalComponent],
   standalone: true,
   templateUrl: './subscribed-services.component.html',
   styleUrl: './subscribed-services.component.scss'
 })
 export class SubscribedServicesComponent {
+  showDatePicker = false;
+  startDate: Date | null = null;
+  endDate: Date | null = null;
 
+  openDatePicker() {
+    this.showDatePicker = true;
+  }
+
+  closeDatePicker() {
+    this.showDatePicker = false;
+  }
+
+  onDateRangeApply(event: { start: Date | null, end: Date | null }) {
+    this.startDate = event.start;
+    this.endDate = event.end;
+    this.showDatePicker = false;
+  }
 }
