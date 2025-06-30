242ae63b7b1fd4ac64dfccde8ed1cd60cdbc5e88
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-06-30 10:23:28 +0600

    service details modal added

---
+++ src/app/components/service-details-modal/service-details-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-service-details-modal',
+  templateUrl: './service-details-modal.component.html',
+  styleUrls: ['./service-details-modal.component.scss'],
+  standalone: true,
+  imports: [NgClass, NgIf]
+})
+export class ServiceDetailsModalComponent {
+  @Input() service: any; 
+  @Output() close = new EventEmitter<void>();
+
+  closeModal() {
+    this.close.emit();
+  }
+
+  get listings() {
+    return [
+      {
+        name: 'Standard Listing',
+        limit: 15,
+        used: 10,
+        color: ''
+      },
+      {
+        name: 'Premium Listing',
+        limit: 15,
+        used: 10,
+        color: 'border-blue-300 bg-blue-50'
+      },
+      {
+        name: 'Premium Plus',
+        limit: 15,
+        used: 10,
+        color: 'border-yellow-200 bg-yellow-50'
+      }
+    ];
+  }
+} 
