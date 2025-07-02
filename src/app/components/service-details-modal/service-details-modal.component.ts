4b06623a762b06406e221c06146424845bcb0b18
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-07-02 11:04:32 +0600

    Service details api

--- src/app/components/service-details-modal/service-details-modal.component.ts
+++ src/app/components/service-details-modal/service-details-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-service-details-modal',
@@ -7,9 +7,17 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule]
 })
-export class ServiceDetailsModalComponent {
+export class ServiceDetailsModalComponent implements OnInit, OnDestroy {
   @Input() service: any; 
   @Output() close = new EventEmitter<void>();
+
+  ngOnInit() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = '';
+  }
 
   closeModal() {
     this.close.emit();
